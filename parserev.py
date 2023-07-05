419907d893d23fb334b73899d411ac81ed959500
Psychowebber <129488372+Psychowebber@users.noreply.github.com>
2023-07-04 20:31:08 -0700

    Update parserev.py
    adding gzip logs support

--- parserev.py
+++ parserev.py
@@ -1,6 +1,7 @@
 import re
 import datetime
 import argparse
+import gzip
 from collections import Counter
 import ipaddress
 
@@ -11,7 +12,7 @@ url_pattern = re.compile(r'\"([^\"]*)\"')
 def parse_log_file(file_path):
     url_hits = Counter()
 
-    with open(file_path, 'r') as file:
+    with open_log_file(file_path) as file:
         for line in file:
             match = url_pattern.search(line)
             if match:
@@ -20,10 +21,17 @@ def parse_log_file(file_path):
 
     return url_hits
 
+# Function to open log files (supports gzip compressed files)
+def open_log_file(file_path):
+    if file_path.endswith('.gz'):
+        return gzip.open(file_path, 'rt')
+    else:
+        return open(file_path, 'r')
+
 # Function to filter log entries based on time range
 def filter_log_entries(file_path, start_time, end_time):
     filtered_entries = []
-    with open(file_path, 'r') as file:
+    with open_log_file(file_path) as file:
         for line in file:
             log_parts = line.split()
             if len(log_parts) < 4:
@@ -48,69 +56,86 @@ def is_matching_ip(ip_address):
 
 # Create the argument parser
 parser = argparse.ArgumentParser(description='Log Analyzer')
-parser.add_argument('--domain', action='store_true', help='Trigger to input the domain name')
-parser.add_argument('--time', type=float, help='Time value')
+parser.add_argument('--domain', required=True, help='Domain name')
+parser.add_argument('--rotated', action='store_true', help='Trigger to search rotated logs with *.gz extension')
+parser.add_argument('--days', type=int, help='Number of rotated logs to search')
 args = parser.parse_args()
 
-# Prompt the user to enter the domain if --domain flag is triggered
-if args.domain:
-    domain = input("Enter the domain: ")
-else:
-    domain = "moodfabrics.com"
+# Construct the log file pattern
+log_file_pattern = "/home/jetrails/{domain}/logs/{domain}-ssl-access_log"
 
-# Construct the log file path
-log_file = "/home/jetrails/{domain}/logs/{domain}-ssl-access_log".format(domain=domain)
+# Check if rotated logs are requested
+if args.rotated:
+    if not args.days:
+        parser.error("--days is required when --rotated is specified")
 
-# Prompt the user to enter the value if --time flag is provided
-if args.time:
-    value = args.time
-else:
+    log_file_pattern += ".{}"
+
+    # Calculate the start and end file numbers based on the number of days
+    start_file_number = 1
+    end_file_number = args.days
+
+# Get the domain name
+domain = args.domain
+
+# Prompt the user to enter the value if --rotated flag is not provided
+if not args.rotated:
     value_str = input("Value Format: 1 = 1 hour ago, 1.15 = 1 hour 15 minutes ago\nEnter the value: ")
     value = float(value_str)
+else:
+    value = None
 
 # Calculate the start and end times based on the value provided
 current_time = datetime.datetime.now()
 start_time = current_time - datetime.timedelta(hours=int(value), minutes=int((value % 1) * 60))
 end_time = current_time
 
-# Parse the log file and get URL hit counts
-url_hits = parse_log_file(log_file)
+# Parse the log files and get URL hit counts
+url_hits = Counter()
+
+if args.rotated:
+    for file_number in range(start_file_number, end_file_number + 1):
+        log_file = log_file_pattern.format(domain, file_number)
+        url_hits += parse_log_file(log_file)
+else:
+    log_file = log_file_pattern.format(domain)
+    url_hits = parse_log_file(log_file)
 
 # Sort URLs by hit count in descending order
 sorted_urls = sorted(url_hits.items(), key=lambda x: x[1], reverse=True)
 
-# Display the top 5 IP addresses by hit number
-print("Top 5 IP Addresses by Hit Number:")
-top_ip_addresses = Counter()
-for i, (url, count) in enumerate(sorted_urls):
-    # Assuming the IP address is the first column in the log file
-    ip_address = url.split()[0]
-    if is_matching_ip(ip_address) and not is_matching_ip(ip_address, "69.27.43.*"):
-        top_ip_addresses[ip_address] += count
-        if len(top_ip_addresses) >= 5:
-            break
+# Display the top 5 URLs and corresponding IP addresses
+print("Top 5 URLs and their hit counts:")
+for i, (url, count) in enumerate(sorted_urls[:5]):
+    print("URL: {}, Hits: {}".format(url, count))
 
-for ip, count in top_ip_addresses.most_common(5):
-    print("IP: {}, Total Hits: {}".format(ip, count))
+    ip_hits = Counter()
 
-    ip_url_hits = Counter()
-    filtered_entries = filter_log_entries(log_file, start_time, end_time)
+    # Filter log entries for the specified time range and check IP addresses
+    if args.rotated:
+        filtered_entries = []
+        for file_number in range(start_file_number, end_file_number + 1):
+            log_file = log_file_pattern.format(domain, file_number)
+            filtered_entries.extend(filter_log_entries(log_file, start_time, end_time))
+    else:
+        filtered_entries = filter_log_entries(log_file, start_time, end_time)
+
     for line in filtered_entries:
         log_parts = line.split()
         if len(log_parts) < 4:
             continue
         entry_ip = log_parts[0]
-        if entry_ip == ip:
+        if is_matching_ip(entry_ip):
             request_method = log_parts[5][1:]
             if request_method not in ("GET", "HEAD"):
                 match = url_pattern.search(line)
                 if match:
                     url = match.group(1)
-                    ip_url_hits[url] += 1
+                    ip_hits[entry_ip] += 1
 
-    top_urls = ip_url_hits.most_common(5)
-    print("Top 5 URLs and Their Hit Counts:")
-    for url, count in top_urls:
-        print("URL: {}, Hits: {}".format(url, count))
+    top_ip_addresses = ip_hits.most_common(5)
+    print("Top 5 IP Addresses:")
+    for ip, ip_count in top_ip_addresses:
+        print("IP: {}, Hits: {} - https://www.abuseipdb.com/check/{}".format(ip, ip_count, ip))
 
     print("---------------------------------")
